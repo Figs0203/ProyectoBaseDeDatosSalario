61a6637df2af3b5ccdc1672d1740740d93a8c0e1
Figs0203 <figuesicsi@gmail.com>
2024-05-11 13:40:03 -0500

    fhsjfdklsjfñslkfjñalfkjdañlfkjdsñk

--- Funciones.py
+++ Funciones.py
@@ -1,41 +1,65 @@
-def bubble_sort(lista):
+def bubble_sort_num(lista, clave):
     length = len(lista)
     for i in range(length):
         for j in range(0, (length - i) - 1):
+            # Obtener los valores asociados con la clave en los diccionarios j y j + 1
+            valor_j = lista[j][clave]
+            valor_j1 = lista[j + 1][clave]
 
-            if lista[j] > lista[j + 1]:
-                auxiliar = lista[j + 1]
-                lista[j + 1] = lista[j]
-                lista[j] = auxiliar
+            # Convertir los valores a cadenas y obtener su longitud
+            valor_j_str = str(valor_j)
+            valor_j1_str = str(valor_j1)
+            len_j = len(valor_j_str)
+            len_j1 = len(valor_j1_str)
+
+            # Comparar los valores teniendo en cuenta su longitud
+            if len_j > len_j1:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+            elif len_j == len_j1 and valor_j > valor_j1:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
     return lista
 
 
-def merge_sort_num(lista):
+def bubble_sort_strings(lista, clave):
+    length = len(lista)
+    for i in range(length):
+        for j in range(0, (length - i) - 1):
+            # Obtener los valores asociados con la clave en los diccionarios j y j + 1
+            valor_j = lista[j][clave]
+            valor_j1 = lista[j + 1][clave]
+
+            # Comparar las cadenas alfabéticamente
+            if valor_j > valor_j1:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+    return lista
+
+
+def merge_sort_num(lista, clave):
     if len(lista) < 2:
         return lista
 
     else:
         middle = len(lista) // 2
-        right = merge_sort_num(lista[:middle])
-        left = merge_sort_num(lista[middle:])
-        return merge_num(right, left)
+        right = merge_sort_num(lista[:middle], clave)
+        left = merge_sort_num(lista[middle:], clave)
+        return merge_num(right, left, clave)
 
 
-def merge_num(lista1, lista2):
+def merge_num(lista1, lista2, clave):
     i, j = 0, 0
     result = []
 
     while i < len(lista1) and j < len(lista2):
         # Comparar por el número de dígitos primero
-        if len(str(lista1[i])) > len(str(lista2[j])):
+        if len(str(lista1[i][clave])) > len(str(lista2[j][clave])):
             result.append(lista1[i])
             i += 1
-        elif len(str(lista1[i])) < len(str(lista2[j])):
+        elif len(str(lista1[i][clave])) < len(str(lista2[j][clave])):
             result.append(lista2[j])
             j += 1
         else:
             # Si tienen el mismo número de dígitos, comparar los valores
-            if lista1[i] > lista2[j]:
+            if lista1[i][clave] > lista2[j][clave]:
                 result.append(lista1[i])
                 i += 1
             else:
@@ -48,24 +72,24 @@ def merge_num(lista1, lista2):
     return result
 
 
-def merge_sort_strings(lista):
+def merge_sort_strings(lista, clave):
     if len(lista) < 2:
         return lista
 
     else:
         middle = len(lista) // 2
-        right = merge_sort_strings(lista[:middle])
-        left = merge_sort_strings(lista[middle:])
-        return merge_strings(right, left)
+        right = merge_sort_strings(lista[:middle], clave)
+        left = merge_sort_strings(lista[middle:], clave)
+        return merge_strings(right, left, clave)
 
 
-def merge_strings(lista1, lista2):
+def merge_strings(lista1, lista2, clave):
     i, j = 0, 0
     result = []
 
     while i < len(lista1) and j < len(lista2):
-        # Comparar las palabras completas
-        if lista1[i] < lista2[j]:
+        # Comparar las palabras completas en función de la clave
+        if lista1[i][clave] < lista2[j][clave]:
             result.append(lista1[i])
             i += 1
         else:
--- Main.py
+++ Main.py
@@ -4,30 +4,113 @@ import Funciones
 archivocsv = open('C:/Users/figue/Desktop/salaries.csv')
 salarios = csv.reader(archivocsv, delimiter=';')
 
-
-trabajo = []
-salario = []
+matriz = []
 
 for fila in salarios:
-    trabajo.append(fila[3])
-    salario.append(fila[6])
+    matriz.append(fila)
 
-trabajo.pop(0)
-salario.pop(0)
+# Crear una lista para almacenar los diccionarios resultantes
+diccionarios = []
 
-salarioordenado = Funciones.merge_sort_num(salario)
-
-trabajoordenado = Funciones.merge_sort_strings(trabajo)
-
+# Iterar sobre las filas de la matriz, excluyendo la primera fila que contiene las claves
+for fila in matriz[1:]:
+    # Crear un diccionario vacío para esta fila
+    diccionario_fila = {}
+    # Iterar sobre los elementos de la primera fila y los elementos de la fila actual
+    for clave, valor in zip(matriz[0], fila):
+        # Asignar el valor actual al diccionario usando la clave correspondiente
+        diccionario_fila[clave] = valor
+    # Agregar el diccionario creado a la lista de diccionarios
+    diccionarios.append(diccionario_fila)
 
 print("-"*275)
 print("=== Menú de opciones ===")
-print("1. Ordenar por residencia")
-print("2. Ordenar por salario")
-print("3. Ordenar por trabajo")
-print("4. Salir\n")
-print("Seleccione el número de la opción que desea ejecutar")
+print("1. Ordenar por residencia (Bubble_sort)")
+print("2. Ordenar por salario (Bubble_sort)")
+print("3. Ordenar por trabajo (Bubble_sort)")
+print("4. Ordenar por residencia (Merge_sort")
+print("5. Ordenar por salario (Merge_sort")
+print("6. Ordenar por trabajo (Merge_sort")
+print("7. Salir\n")
 
-respuesta = int(input())
+respuesta = int(input("Seleccione el número de la opción que desea ejecutar: "))
+
+if respuesta == 1:
+    dic_residence = Funciones.bubble_sort_strings(diccionarios, "employee_residence")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_residence:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
 
 
+elif respuesta == 2:
+    dic_salary = Funciones.bubble_sort_num(diccionarios, "salary_in_usd")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_salary:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
+
+
+elif respuesta == 3:
+    dic_job = Funciones.bubble_sort_strings(diccionarios, "job_title")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_job:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
+
+elif respuesta == 4:
+    dic_residence = Funciones.merge_sort_strings(diccionarios, "employee_residence")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_residence:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
+
+elif respuesta == 5:
+    dic_salary = Funciones.merge_sort_num(diccionarios, "salary_in_usd")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_salary:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
+
+elif respuesta == 6:
+    dic_job = Funciones.merge_sort_strings(diccionarios, "job_title")
+    with open('salida.txt', 'w', encoding='utf-8') as archivo:
+        for diccionario in dic_job:
+            # Redirige la salida de print() al archivo
+            print(f"Año: {diccionario["work_year"]} | Nivel de experiencia: {diccionario["experience_level"]} "
+                  f"| Tipo de empleo: {diccionario["employment_type"]} | Trabajo: {diccionario["job_title"]} "
+                  f"| Salario (en usd): {diccionario["salary_in_usd"]} "
+                  f"| Residencia del empleado: {diccionario["employee_residence"]} "
+                  f"| Ubicación de la compañía: {diccionario["company_location"]} "
+                  f"| Tamaño de la compañía: {diccionario["company_size"]}\n", file=archivo)
+
+elif respuesta == 7:
+    print("Adiós")
+
+else:
+    print("Ingrese un caracter válido")
